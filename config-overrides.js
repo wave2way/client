9dbabdf56a6bd48a6fc800bf3d67cc527d4f5a4d
monster <ynhmonster@163.com>
2019-05-11 00:59:02 +0800

    update

--- config-overrides.js
+++ config-overrides.js
@@ -1,19 +1,20 @@
-const { override, fixBabelImports, addLessLoader } = require('customize-cra');
+const { override, fixBabelImports, addLessLoader, disableChunk } = require('customize-cra');
 
 module.exports = override(
+  disableChunk(),
   fixBabelImports('import', {
     libraryName: 'antd',
      libraryDirectory: 'es',
      style: 'true',
-   }),
-   addLessLoader({
-        javascriptEnabled: true,
-        modifyVars: { 
-            '@primary-color': '#1890ff',
-            '@link-color': "#1890ff",
-            "@heading-color": "#FFF",
-            "@border-radius-base": "8px",
-            "@font-size-base": "14px"
-        },
-    }),
+  }),
+  addLessLoader({
+    javascriptEnabled: true,
+    modifyVars: { 
+      '@primary-color': '#1890ff',
+      '@link-color': "#1890ff",
+      "@heading-color": "#FFF",
+      "@border-radius-base": "8px",
+      "@font-size-base": "14px"
+    },
+  }),
 );
